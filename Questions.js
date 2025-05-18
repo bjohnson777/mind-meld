27d2e553c8fdfcdf7da2ff39c04ea26dfe895875
Barbara Johnson <bjdesignsit@icloud.com>
2025-05-18 16:09:33 -0500

    Create Questions.js
    Added on May 18th, 2025

---
+++ Questions.js
@@ -0,0 +1,7 @@
+[
+  { "question": "What color comes to your mind when I say 'power'?", "answer": "red" },
+  { "question": "What is the first word you associate with 'truth'?", "answer": "light" },
+  { "question": "Which emotion do you feel when you hear 'machine'?", "answer": "curiosity" },
+  { "question": "What object represents destiny to you?", "answer": "compass" },
+  { "question": "What flavor describes your mood today?", "answer": "spicy" }
+]
